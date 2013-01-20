bb11dec2201b07536bf57db0ae959c3f767fb293
Aayush Upadhyay <aayushu@gmail.com>
2013-01-20 05:39:18 -0500

    updated scraper

--- scraper.py
+++ scraper.py
@@ -14,7 +14,6 @@ mongo_conn = pymongo.Connection('localhost:27017')
 singer_coll = mongo_conn['artists']['singer']
 
 fb_graph = facepy.GraphAPI()
-
 sc_client = soundcloud.Client(client_id='15796297f59f225886f6247ba56a1a43')
 
 #spotify = SpotifyApi()
@@ -22,20 +21,25 @@ gc = grooveshark.Client()
 gc.init()
 
 singer_song_map = {}
+eval_lst = ['Adele']
 
 def urlify (name):
     return name.replace(' ', '+')
 
-def process_singer(name, level): 
 
+def get_stats(name):
     name = name.encode('ascii', 'ignore')
 
     if name in singer_song_map:
-        return singer_song_map[name]
+        return (singer_song_map[name], [])
 
-    #We've gone 5 levels done, just keep going 
-    if level < 0:
-        return None
+    cursor = singer_coll.find({'name':name})
+
+    #value's in database, don't recompute
+    if cursor.count() > 0:
+        item = cursor.next()
+        peers = item['peers'].keys()
+        return (item, peers)
 
     soup = ""
     try: 
@@ -51,7 +55,6 @@ def process_singer(name, level):
         if entry.get('class') == ['S']:
             if entry.string != name:
                 peers.append(entry.string)
-
 
     try:
         song_search = gc.search(name)
@@ -84,8 +87,8 @@ def process_singer(name, level):
         desc = wikipedia.getArticle(name)
     except:
         desc = name
-    wiki_url = "http://en.wikipedia.org/w/index.php?title=%s" % name.replace(' ', '_')
 
+    wiki_url = "http://en.wikipedia.org/w/index.php?title=%s" % name.replace(' ', '_')
     sc_url = "http://soundcloud.com"
 
     try:
@@ -106,54 +109,51 @@ def process_singer(name, level):
     item['song_pop'] = song_pop
     item['song_id'] = stream_id
     item['song_cover'] = song_cover_url
-    singer_song_map[name] = item 
-
     item['peers'] = {}
 
-    print 'Added singer %s to database' % item['name']
-    print 'Image URL is %s' % item['song_cover']
-    try:
-        singer_coll.update({'_id': _id}, item, upsert=True)
-    except:
-        pass
+    singer_song_map[name] = item
+    return (item, peers)
 
-    #Go a level down 
+def process_singer(name): 
+
+    try:
+        item,peers = get_stats(name)
+    except ValueError:
+        return
+
     for peer in peers:
         if peer not in singer_song_map:
-            peer_data = process_singer(peer, level-1)
+            eval_lst.append(peer)
+            try:
+                it, pr = get_stats(peer)
+                item['peers'][peer] = it 
+            except ValueError:
+                pass
 
-            if not peer_data:
-                continue
-
-            peer_data['peers'] = {}
-            
-            item['peers'][peer] = peer_data 
-            singer_song_map[peer] = peer_data
-
-        #peer's already there, let's look up and get the statistics in a global structure we've saved
-        #only using global structure to save mongo queries
         else:
             peer_data = singer_song_map[peer]
-            peer_data['peers'] = {}
             item['peers'][peer] = peer_data
 
+    _id = item['_id']
     cursor = singer_coll.find({'_id':_id})
+
+    if cursor.count() == 0:
+        print 'Inserting %s' % item['name']
+        singer_coll.update({'_id': _id}, item, upsert=True)
+        return 
+
     doc = cursor.next()
-
     if len(doc['peers']) <= len (item['peers']):
-        try:
-            singer_coll.update({'_id': _id}, item, upsert=True)
-        except:
-            pass
+        print 'Inserting %s' % item['name']
+        singer_coll.update({'_id': _id}, item, upsert=True)
 
-    return item 
 
-def add_singers(singers):
+def add_singers():
 
-    for singer in singers:
-        process_singer(singer, 5)
+    while len(eval_lst) > 0:
+        singer = eval_lst.pop(0)
+        process_singer(singer)
 
 if __name__ == '__main__':
-    singers = ['Eminem']
-    add_singers(singers)
+    add_singers()
 
